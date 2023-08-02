aed8a69b30ef30f6fcd142283002afee1500149a
krishna-123-star <68377201+krishna-123-star@users.noreply.github.com>
2023-08-02 15:38:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const getType = arg => typeof arg 
+
+console.log("The type of  42 is : " + getType(42))
+console.log("The type of false is : "+getType(false))
+console.log("The type of Krishna is: "+getType("Krishna"))
+console.log("The type of  {} is: "+getType({}))
